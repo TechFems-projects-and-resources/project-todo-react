a040f84a2c8c26fb18f5792c5ebdb114db5a15f3
Jethet <henriette.hettinga@protonmail.com>
2022-01-19 12:06:08 +0100

    Use date and time as key value to set local storage

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,24 +2,30 @@ import React, { useState } from "react";
 
 const ToDo = () => {
   const [card, setCard] = useState({ title: "", comment: "" });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(card);
-    
-  };
+  // HOW DO I GET A KEY FOR LOCAL STORAGE - USE DATE&TIME?
+  const [key, setKey] = useState(new Date())
 
   // With hooks, the old state is REPLACED by the one that triggers the event.
   // To correct that, you’ll need to copy the entire properties from the old state
   // using the spread operator (…) and override the part of it that is the new.
-
   const handleChange = (e) => {
     setCard({
       ...card,
       [e.target.name]: e.target.value,
-    });    
+    });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(card);
+    setKey(new Date())
+    localStorage.setItem(JSON.stringify(key), JSON.stringify(card));
+    setTimeout(resetCard, 4000)
+  };
+
+  const resetCard = () => {
+    setCard({ title: "", comment: "" })
+  }
 
   return (
     <div className="todo-card">
