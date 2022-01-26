a02df7a60e2f1eede239a549ab4ec6d138eac1ad
Jethet <henriette.hettinga@protonmail.com>
2022-01-26 21:11:35 +0100

    Clean up code

--- src/components/ToDoOverview.jsx
+++ src/components/ToDoOverview.jsx
@@ -5,20 +5,18 @@ const ToDoOverview = (props) => {
   return (
     <div className="overview">
       <h1>Get all To Do cards in the overview</h1>
+      
       <ToDo />
 
       {props.todoList.map((card, index) => {
         return (
           <div key={index}>
-          <p>Title: {card.title}</p>
-          <p>Description: {card.description}</p>
-          <p>Tags: {card.tags}</p>
-        </div>
-        )
-        
+            <p>Title: {card.title}</p>
+            <p>Description: {card.description}</p>
+            <p>Tags: {card.tags}</p>
+          </div>
+        );
       })}
-
-
     </div>
   );
 };
