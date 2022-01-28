dcb2b373a7b4d711d9b907945e1735a0fdfacc3e
Jethet <henriette.hettinga@protonmail.com>
2022-01-28 12:56:11 +0100

    Use Switch; add buttons navigation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom"
-import ToDoOverview from "./components/ToDoOverview";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./components/Homepage";
+// import ToDoOverview from "./components/ToDoOverview";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-
-  const getTodoStorage = () => {
-    let cards = [];
-    let keys = Object.keys(localStorage);
-    let i = keys.length;
-
-    while (i--) {
-      cards.push(JSON.parse(localStorage.getItem(keys[i])));
-    }
-    return cards;
-  };
-
-  useEffect(() => {
-    setTodoList(getTodoStorage());
-  }, []);
-
-  useEffect(() => {
-    console.log(todoList);
-  }, [todoList])
 
   return (
-    <Router>
-    <div className="App">
-      <div>
-        <h1>This is a todo app, created with React</h1>
-      </div>
-      <div>
-        <ToDoOverview todoList={todoList} />
-      </div>
-    </div>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={Homepage} />
+    </Switch>
   );
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import ToDoOverview from "./ToDoOverview";
 
 const Homepage = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const getTodoStorage = () => {
+    let cards = [];
+    let keys = Object.keys(localStorage);
+    let i = keys.length;
+
+    while (i--) {
+      cards.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    return cards;
+  };
+
+  useEffect(() => {
+    setTodoList(getTodoStorage());
+  }, []);
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
   return (
-  <div></div>
-  )
-}
+    <div>
+      <h1>This is a todo app, created with React</h1>
 
+      <div>
+        <ToDoOverview todoList={todoList} />
+      </div>
+      <div>
+        <button onClick={() => history.push("/todo")}>Create new To Do</button>
+        <button onClick={() => history.push("/todooverview")}>To Do Overview</button>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
