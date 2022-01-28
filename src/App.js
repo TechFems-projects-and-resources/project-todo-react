dadac0a5339bf216420bd13020dfb63754a11ba0
Jethet <henriette.hettinga@protonmail.com>
2022-01-28 15:23:04 +0100

    Add error page; styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
-// import ToDoOverview from "./components/ToDoOverview";
+import ToDoOverview from "./components/ToDoOverview";
+import ToDo from "./components/ToDo";
+import Error from "./components/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/todo" element={<ToDo />} />
+        <Route path="/todooverview" element={<ToDoOverview />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error = () => {
+  
+  return (
+    <div className="error-message">
+      <p>Unfortunately this page does not exist.</p>
+      <p>You can go to the homepage, the To Do page or the To Do Overview page.</p>
+    </div>
+  );
+};
+
+export default Error;
