fbbf039e5edaa9b3ae4d0057765b5e3068b49c94
Jethet <henriette.hettinga@protonmail.com>
2022-01-30 17:02:09 +0100

    Fix CSS error page; finalise ToDoOverview page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,29 +1,10 @@
-import { useState, useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
 import ToDoOverview from "./ToDoOverview";
 
 const Homepage = () => {
-  const [todoList, setTodoList] = useState([]);
   const navigate = useNavigate();
 
-  const getTodoStorage = () => {
-    let cards = [];
-    let keys = Object.keys(localStorage);
-    let i = keys.length;
-
-    while (i--) {
-      cards.push(JSON.parse(localStorage.getItem(keys[i])));
-    }
-    return cards;
-  };
-
-  useEffect(() => {
-    setTodoList(getTodoStorage());
-  }, []);
-
-  useEffect(() => {
-    console.log(todoList);
-  }, [todoList]);
+  
 
   return (
     <div className="homepage">
--- src/components/ToDoOverview.jsx
+++ src/components/ToDoOverview.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import { useState, useEffect} from "react";
 import ToDo from "./ToDo";
 
-const ToDoOverview = (props) => {
+const ToDoOverview = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const getTodoStorage = () => {
+    let cards = [];
+    let keys = Object.keys(localStorage);
+    let i = keys.length;
+
+    while (i--) {
+      cards.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    return cards;
+  };
+
+  useEffect(() => {
+    setTodoList(getTodoStorage());
+  }, []);
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
   return (
     <div className="overview">
       <h1>Get all To Do cards in the overview</h1>
-      
-      <ToDo />
       <div className="card-list">
-      {props.todoList.map((card, index) => {
-        return (
-          <div className="overview-card" key={index}>
-            <p>Title: {card.title}</p>
-            <p>Description: {card.description}</p>
-            <p>Tags: {card.tag}</p>
-          </div>
-        );
-      })}
+        {todoList.map((card, index) => {
+          return (
+            <div className="overview-card" key={index}>
+              <p>Title: {card.title}</p>
+              <p>Description: {card.description}</p>
+              <p>Tags: {card.tag}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
