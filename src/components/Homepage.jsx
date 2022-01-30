bb01bf649c6cc518eac8cc5837368bf6e8bef9d0
Jethet <henriette.hettinga@protonmail.com>
2022-01-30 17:03:16 +0100

    Clean up code after reshuffling components

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import ToDoOverview from "./ToDoOverview";
 
 const Homepage = () => {
   const navigate = useNavigate();
 
-  
-
   return (
     <div className="homepage">
       <h1>This is a todo app, created with React</h1>
-      <div>
-        {/* <ToDoOverview todoList={todoList} /> */}
-      </div>
       <div className="button-set">
         <button className="nav-button" onClick={() => navigate("/todo")}>Create new To Do</button>
         <button className="nav-button" onClick={() => navigate("/todooverview")}>To Do Overview</button>
--- src/components/ToDoOverview.jsx
+++ src/components/ToDoOverview.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect} from "react";
-import ToDo from "./ToDo";
 
 const ToDoOverview = () => {
   const [todoList, setTodoList] = useState([]);
